147ffa54ac7dc7f37e2cf1f16175c5fde40c1888
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-12-17 20:16:01 +0100

    Fix day 15 part 2

--- src/day15.rs
+++ src/day15.rs
@@ -38,44 +38,31 @@ impl PartialOrd for State {
     }
 }
 
-fn tile_risk(risk: &Input, tile: usize) -> Input {
-    if tile == 1 {
-        return risk.clone();
+fn tiled_risk(row: usize, col: usize, n_tiles: usize, risk: &Input) -> u64 {
+    if n_tiles == 1 {
+        return risk[row][col] as u64;
     }
 
     let n_rows = risk.len();
-    let n_rows_tiled = n_rows * tile;
     let n_cols = risk[0].len();
-    let n_cols_tiled = n_cols * tile;
 
-    let mut risk_tiled = vec![vec![0; n_cols_tiled]; n_rows_tiled];
+    let row_tile = (row / n_rows) as u64;
+    let col_tile = (col / n_cols) as u64;
 
-    for row in 0..n_rows_tiled {
-        let row_tile = (row / n_rows) as u8;
+    let base_risk = risk[row % n_rows][col % n_cols] as u64;
+    let new_risk = base_risk + row_tile + col_tile;
 
-        for col in 0..n_cols_tiled {
-            let col_tile = (col / n_cols) as u8;
-
-            let base_risk = risk[row % n_rows][col % n_cols];
-            let new_risk = base_risk + row_tile + col_tile;
-
-            risk_tiled[row][col] = if new_risk > 9 {
-                new_risk % 10 + 1
-            } else {
-                new_risk
-            };
-        }
+    if new_risk > 9 {
+        return new_risk % 10 + 1;
     }
 
-    risk_tiled
+    new_risk
 }
 
 
-fn dijkstra(risk: &Input, tile: usize) -> u64 {
-    let risk = tile_risk(risk, tile);
-    println!("Grid size: {}x{}", risk.len(), risk[0].len());
-    let n_rows = risk.len();
-    let n_cols = risk[0].len();
+fn dijkstra(risk: &Input, n_tiles: usize) -> u64 {
+    let n_rows = risk.len() * n_tiles;
+    let n_cols = risk[0].len() * n_tiles;
 
     let mut dist = vec![vec![u64::MAX; n_cols]; n_rows];
 
@@ -88,9 +75,10 @@ fn dijkstra(risk: &Input, tile: usize) -> u64 {
     dist[0][0] = 0;
 
     while let Some(State{cost, position}) = heap.pop() {
+        let (row, col) = position;
+
         if position == target {return cost; }
 
-        let (row, col) = position;
 
         if cost > dist[row][col] { continue; }
 
@@ -99,18 +87,18 @@ fn dijkstra(risk: &Input, tile: usize) -> u64 {
                 continue;
             }
 
-            let neighbor_row = (row as i64 + drow as i64) as usize;
-            let neighbor_col = (col as i64 + dcol as i64) as usize;
-            let neighbor_cost = risk[neighbor_col][neighbor_col] as u64;
+            let next_row = (row as i64 + drow as i64) as usize;
+            let next_col = (col as i64 + dcol as i64) as usize;
+            let next_cost = tiled_risk(next_row, next_col, n_tiles, risk);
 
             let next = State{
-                cost: cost + neighbor_cost,
-                position: (neighbor_row, neighbor_col),
+                cost: cost + next_cost,
+                position: (next_row, next_col),
             };
 
-            if next.cost < dist[neighbor_row][neighbor_col] {
+            if next.cost < dist[next_row][next_col] {
                 heap.push(next);
-                dist[neighbor_row][neighbor_col] = next.cost;
+                dist[next_row][next_col] = next.cost;
             }
         }
     }
@@ -129,10 +117,10 @@ fn part2(input: &Input) -> u64 {
 
 pub fn day15() {
     let input = input::get_input(15);
-    let risk_levels = parse_input(&input);
+    let risk = parse_input(&input);
 
     let t0 = Instant::now();
-    println!("Part1: {}", part1(&risk_levels));
-    println!("Part2: {}", part2(&risk_levels));
+    println!("Part1: {}", part1(&risk));
+    println!("Part2: {}", part2(&risk));
     println!("Time: {} us", t0.elapsed().as_micros());
 }
